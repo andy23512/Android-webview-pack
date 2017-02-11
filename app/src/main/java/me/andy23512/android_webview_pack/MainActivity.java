1408d73e215a54216a28c346fe88ae56e010e2b7
andy23512 <andy23512@gmail.com>
2017-02-11 19:48:30 +0800

    set fullscreen webview

---
+++ app/src/main/java/me/andy23512/android_webview_pack/MainActivity.java
@@ -0,0 +1,28 @@
+package me.andy23512.android_webview_pack;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView wv;
+    private String urlStart = "http://github.com/andy23512";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView) findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.clearCache(true);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl(urlStart);
+    }
+}
